919b6c1a9549299dd25e8d950f30aefb910218da
balireza27 <balireza3040@gmail.com>
2024-06-27 15:35:52 +0330

    first page

--- Final/script.js
+++ Final/script.js
@@ -1,1 +1,12 @@
+const form = document.getElementById("login-form");
+const passwordInput = document.getElementById("password");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const password = passwordInput.value;
+  if (password === "12345678") {
+    window.location.href = "sucsess.html";
+  } else {
+    alert("Invalid password. Please try again.");
+  }
+});
